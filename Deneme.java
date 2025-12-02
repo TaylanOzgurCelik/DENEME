cf00c4c21ab864e623d74d049a40f878b2310dfe
TaylanOzgurCelik <taylanozgurcelikk@gmail.com>
2025-12-02 09:19:56 +0300

    Update Deneme.java

--- Deneme.java
+++ Deneme.java
@@ -1,8 +1,30 @@
+import java.util.Random;
+
 public class Deneme {
     
 
+
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+    }
+
+    
+    public static int[] createRandomColor(){
+
+        Random rn=new Random();
+        int r=rn.nextInt(256);
+        int g=rn.nextInt(256);
+        int b=rn.nextInt(256);
+        int[] color= {r,g,b};
+        return color;
+    }
+    
+    public static int[][] redify(int[][] input, int index){
+
+
+        
+
+        return null;
     }
 
     
